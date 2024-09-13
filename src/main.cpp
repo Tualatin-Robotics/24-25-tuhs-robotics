36a34b767dda0eb66bf02426e6e7b0226e49fd26
QuasarK <96966687+quasark-main@users.noreply.github.com>
2024-09-12 17:17:06 -0700

    init

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include "main.h"
+
+const float MOVE_VOLT = 12000.0 / 128.0;
+
+void initialize() {
+	
+}
+
+void autonomous() {
+
+}
+
+void opcontrol() {
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+	pros::Motor left_front(11);
+	pros::Motor right_front(12);
+
+	while (true) {
+		int left = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+		int right = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+
+		left_front.move_voltage(left * MOVE_VOLT);
+		right_front.move_voltage(right * -MOVE_VOLT);
+
+
+		pros::delay(20);
+	}
+}
