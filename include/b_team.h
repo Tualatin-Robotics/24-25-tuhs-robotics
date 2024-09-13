f50d7722892c043d754bbe868bbf2ba54f1ec57d
SoupremeChickn <soupreme.chickn@gmail.com>
2024-09-12 17:32:47 -0700

    added secondary file to call function from

---
+++ include/b_team.h
@@ -0,0 +1,21 @@
+#include "main.h"
+
+const float MOVE_VOLT = 12000.0 / 128.0;
+
+void drive() {
+    pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+	pros::Motor left_front(11);
+	pros::Motor right_front(12);
+
+	while (true) {
+		int left = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+		int right = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+
+		left_front.move_voltage(left * MOVE_VOLT);
+		right_front.move_voltage(right * -MOVE_VOLT);
+
+
+		pros::delay(20);
+	}
+}
