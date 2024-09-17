a89d457b78206ab139cdfe2910d0948497da09d3
SoupremeChickn <soupreme.chickn@gmail.com>
2024-09-16 17:10:14 -0700

    mutliple files for each team

--- include/c_team.h
+++ include/c_team.h
@@ -1,7 +1,6 @@
 #include "main.h"
 #include "variables.h"
 
-
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 
 pros::Motor left_front(2);
@@ -9,11 +8,12 @@ pros::Motor right_front(1);
 pros::Motor left_back(4);
 pros::Motor right_back(3);
 
-
-void b_drive() {
+void c_drive() {
 	int left = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 	int right = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
 
 	left_front.move_voltage(left * MOVE_VOLT);
 	right_front.move_voltage(right * -MOVE_VOLT);
+
+	pros::delay(20);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
 #include "main.h"
+#include "a_team.h"
 #include "b_team.h"
+#include "c_team.h"
+#include "variables.h"
 
-
+int team = TEAM_MEDIA;
 
 void initialize() {
 	
 }
 
 void autonomous() {
-
+	
 }
 
 void opcontrol() {
-	drive();
+	while (true) {
+		switch (team) {
+			case TEAM_A:
+				a_drive();
+			case TEAM_B:
+				b_drive();
+			case TEAM_C:
+				c_drive();
+		}
+		pros::delay(20);
+	}
 }
