22883fc7e061c06ae4775ee453a0d714f81e45e8
benjaminthemagnificent2024 <sthomeschool8@gmail.com>
2024-09-19 17:16:30 -0700

    Bot go all over the place when buttons pressed

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,15 @@ void opcontrol() {
 	while (true) {
 		int left = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 		int right = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-		bool buttonPressed=master.get_digital(pros::E_CONTROLLER_DIGITAL_A);
+		bool a_buttonPressed=master.get_digital(pros::E_CONTROLLER_DIGITAL_A);
+		bool b_buttonPressed=master.get_digital(pros::E_CONTROLLER_DIGITAL_B);
+		bool x_buttonPressed=master.get_digital(pros::E_CONTROLLER_DIGITAL_X);
+		bool y_buttonPressed=master.get_digital(pros::E_CONTROLLER_DIGITAL_Y);
 		int thing=1;
-		if(buttonPressed) thing=-1;
-		//int thing=(buttonPressed)-1:1;
+		if(a_buttonPressed) thing*=-1;
+		if(b_buttonPressed) thing*=-1;
+		if(x_buttonPressed) thing*=-1;
+		if(y_buttonPressed) thing*=-1;
 
 		left_front.move_voltage(left * MOVE_VOLT * thing);
 		right_front.move_voltage(right * -MOVE_VOLT * thing);
