adca4ff9ff75d18a23f910818e56037f0f64fe21
HackerMagnificent <sthomeschool8@gmail.com>
2024-10-27 16:16:38 -0700

    added features in c_team file
    Controller input logging and parsing functions, once tested, I will add them to the main file.

--- include/c_team.h
+++ include/c_team.h
@@ -1,3 +1,6 @@
+#include <fstream>
+#include <cstring>
+using namespace std;
 #include "main.h"
 #include "variables.h"
 
@@ -10,7 +13,9 @@ pros::Motor right_back(17);
 
 void auton() {
 	while(true){
-		doStuff();//everything is false right now
+		int a[16];
+		/*parseFileInput("c_team_auton_file.txt",a,0);
+		doStuff(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9],a[10],a[11],a[12],a[13],a[14],a[15]);*/
 		pros::delay(2000);//2 seconds
 		//this will later be mathematically aligned with the speed at which the controller input is recorded
 	}
@@ -35,15 +40,43 @@ void drive() {
 		master.get_digital(pros::E_CONTROLLER_DIGITAL_R1),
 		master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)
 	);
-	//doStuff(Ctrlr(0,0,0,0,true,false,false,false,false,false,false,false));
+	
 }
 
 void doStuff(int leftX=0, int leftY=0, int rightX=0, int rightY=0, 
-bool a=false, bool b=false, bool x=false, bool y, 
-bool right=false,bool down=false, bool up=false, bool left=false,
-bool l1=false, bool l2=false, bool r1=false, bool r2=false){
+	bool a=false, bool b=false, bool x=false, bool y, 
+	bool right=false,bool down=false, bool up=false, bool left=false,
+	bool l1=false, bool l2=false, bool r1=false, bool r2=false){
 	//all of the functionality code for the bots goes here
 	left_front.move_voltage((leftX * MOVE_VOLT * 0.5 *a) + (a*6000));
 	right_front.move_voltage((leftX * -MOVE_VOLT * 0.5 *a) + (a*6000));
 	//button input will eventually be recorded here
+	//uncomment ln55 to test logInputs()
+	//logInputs("c_team_auton_file.txt",leftX,leftY,rightX,rightY,a,b,x,y,right,down,up,left,l1,l2,r1,r2);
 };
+
+void logInputs(string fileName,int leftX=0, int leftY=0, int rightX=0, int rightY=0, 
+	bool a=false, bool b=false, bool x=false, bool y=false, 
+	bool right=false,bool down=false, bool up=false, bool left=false,
+	bool l1=false, bool l2=false, bool r1=false, bool r2=false){
+		ofstream theFile(fileName);
+		theFile<<leftX<<","<<leftY<<","<<rightX<<","<<rightY<<","<<a<<","<<b<<","<<x<<","<<y<<","<<right<<","<<down<<","<<up<<","<<left<<","<<l1<<","<<l2<<","<<r1<<","<<r2<<"\n";
+		theFile.close();
+}
+
+void parseFileInput(string fileName,int array[16],int lineNumber){//this probably won't work as expected so be careful when testing
+	ifstream theFile(fileName);
+	string thing;
+	getline(theFile,thing);//this is easier than converting a string to a char array
+	char line[thing.length()];
+	theFile.getline(line,thing.length());//still need to figure out how to get appropriate line of file
+	theFile.close();
+	char* t=strtok(line,",");
+	int number=0;
+	while(t!=NULL){
+		array[number]=(int)t;//functions cannot return arrays, so this one modifies an array passed as an argument
+		number++;
+		t=strtok(NULL,",");
+	}
+	//so far, this entire function should retrieve the first line of the file, then parse it and put it into an int array
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,31 @@
-#include "main.h"
+//Change team here:
+#define B_TEAM;
 
-const float MOVE_VOLT = 12000.0 / 128.0;
+#ifdef A_TEAM;
+#include "a_team.h"
+#endif
+
+#ifdef B_TEAM;
+#include "b_team.h"
+#endif
+
+#ifdef C_TEAM;
+#include "c_team.h"
+#endif
+
+//int team = TEAM_C;
 
 void initialize() {
 	
 }
 
 void autonomous() {
-
+	auton();
 }
 
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-
-	pros::Motor left_front(11);
-	pros::Motor right_front(12);
-	int direction=0;
-	bool throwing=false;
 	while (true) {
-		int left = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-		int right = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-
-		left_front.move_voltage(left * MOVE_VOLT);
-		right_front.move_voltage(right * -MOVE_VOLT);
-
-		int leftStick = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
-		int rightStick = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-		bool a_buttonPressed=master.get_digital(pros::E_CONTROLLER_DIGITAL_A);
-		bool b_buttonPressed=master.get_digital(pros::E_CONTROLLER_DIGITAL_B);
-		bool x_buttonPressed=master.get_digital(pros::E_CONTROLLER_DIGITAL_X);
-		bool y_buttonPressed=master.get_digital(pros::E_CONTROLLER_DIGITAL_Y);
-		bool l_buttonPressed=master.get_digital(pros::E_CONTROLLER_DIGITAL_L1) || master.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
-		bool r_buttonPressed=master.get_digital(pros::E_CONTROLLER_DIGITAL_R1) || master.get_digital(pros::E_CONTROLLER_DIGITAL_R2);
-
-		
-		if(a_buttonPressed){//accelerate
-			if(leftStick>0){//right
-				left_front.move_voltage(127 * MOVE_VOLT);
-				right_front.move_voltage((127-leftStick) * -MOVE_VOLT);//slow down right wheel
-				direction=1;
-			}
-			else if(leftStick<0){//left
-				right_front.move_voltage(-127 * MOVE_VOLT);
-				left_front.move_voltage((127-leftStick) * MOVE_VOLT);//slow down left wheel
-				direction=-1;
-			}
-			else{//straight
-				direction=0;
-				left_front.move_voltage(127 * MOVE_VOLT);
-				right_front.move_voltage(-127 * MOVE_VOLT);
-			}
-			if(direction && r_buttonPressed){//drift
-				if(direction==1) right_front.move_voltage(0);//brake right motor, is there a brake() function?
-				if(direction==-1) left_front.move_voltage(0);//brake left motor
-			}
-		}
-		else if(b_buttonPressed){//brake / go backwards
-			left_front.move_voltage(-127 * MOVE_VOLT);
-			right_front.move_voltage(127 * MOVE_VOLT);
-		}
-		else{
-			right_front.move_voltage(0);
-			left_front.move_voltage(0);
-		}
-
-		if(l_buttonPressed){//"throw"??
-			if(!throwing) throw(rightStick>0);
-			throwing=true;
-		}
-		else{
-			throwing=false;
-		}
-
-		void throw(backwards){
-			print("throwing shell");
-			if(backwards) print(" backwards");
-		}
-		
-
+		drive();
 
 		pros::delay(20);
 	}
