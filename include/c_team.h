329855df2f773efe10d4fc5089d98631fede5465
Benjamin <sthomeschool8@gmail.com>
2024-10-31 16:39:08 -0700

    Adjustments in c_team file to be tested (#5)
    * Way to confuse the driver
    
    * Bot go backward if A get pressed
    
    * Bot go all over the place when buttons pressed
    
    * Make Less Confusing
    
    * Mario Kart Controls
    
    Implements the controls from the game "Mario Kart 8 Deluxe" on the Nintendo Switch.
    A: Accelerate,
    B: Brake / Drive Backwards,
    Left Control Stick (Y): Steer,
    R / R2: "Drift",
    L / L2: "Throw Item"
    
    * Changed structure of c_team
    
    Made a more flexible system for handling button inputs. opcontrol() and autonomous() now draw from the same function (doStuff() creative name lol) but pass in different arguments, opcontrol() using live input from master, while autonomous() will pass pre-recorded values for the buttons (recording system yet to be rigged). I can copy this into a_team and b_team if it works.
    
    * added features in c_team file
    
    Controller input logging and parsing functions, once tested, I will add them to the main file.
    
    * messed around with some stuff
    
    * changed some more stuff
    
    * more adjustments to file reading system
    
    * minor changes

--- include/c_team.h
+++ include/c_team.h
@@ -1,3 +1,6 @@
+#include <fstream>
+#include <cstring>
+using namespace std;
 #include "main.h"
 #include "variables.h"
 
@@ -9,28 +12,70 @@ pros::Motor left_back(18);
 pros::Motor right_back(17);
 
 void auton() {
-	left_front.move_voltage(127.0 * MOVE_VOLT);
-	right_front.move_voltage(127.0 * -MOVE_VOLT);
-
-	pros::delay(2000);
-
-	left_front.move_voltage(0);
-	right_front.move_voltage(0);
-
-	pros::delay(2000);
-
-	left_front.move_voltage(127.0 * MOVE_VOLT);
-
-	pros::delay(500);
-
-	left_front.move_voltage(0);
+	int frame=0;
+	while(true){
+		int a[16];
+		//uncomment ln18-19 to test parseFileInput() once c_team_auton_file.txt has information that should look something like: "127,0,-127,0,1,1,0,1,0,0,0,0,1,1,0,1" (actual values may vary)
+		/*parseFileInput("c_team_auton_file.txt",a,frame);
+		doStuff(a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9],a[10],a[11],a[12],a[13],a[14],a[15]);*/
+		frame++;
+		pros::delay(2000);//2 seconds, this will later be mathematically aligned with the speed at which the controller input is recorded
+	}
 }
 
 void drive() {
-	int left = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-	int right = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-	bool b = master.get_digital(pros::E_CONTROLLER_DIGITAL_B);
+	doStuff(
+		master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X),
+		master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y),
+		master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X),
+		master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_A),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_B),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_X),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_Y),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_UP),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_L1),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_L2),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_R1),
+		master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)
+	);
+	
+}
 
-	left_front.move_voltage(left * MOVE_VOLT * 0.5 *!b);
-	right_front.move_voltage(right * -MOVE_VOLT * 0.5*!b);
+void doStuff(int leftX=0, int leftY=0, int rightX=0, int rightY=0, 
+	bool a=false, bool b=false, bool x=false, bool y, 
+	bool right=false,bool down=false, bool up=false, bool left=false,
+	bool l1=false, bool l2=false, bool r1=false, bool r2=false){
+	//all of the functionality code for the bots goes here
+	left_front.move_voltage((leftX * MOVE_VOLT * 0.5 *a) + (a*6000));
+	right_front.move_voltage((leftX * -MOVE_VOLT * 0.5 *a) + (a*6000));
+	//button input will eventually be recorded here
+	//uncomment ln57 to test logInputs()
+	//logInputs("c_team_auton_file.txt",leftX,leftY,rightX,rightY,a,b,x,y,right,down,up,left,l1,l2,r1,r2);
+};
+
+void logInputs(string fileName,int leftX=0, int leftY=0, int rightX=0, int rightY=0, 
+	bool a=false, bool b=false, bool x=false, bool y=false, 
+	bool right=false,bool down=false, bool up=false, bool left=false,
+	bool l1=false, bool l2=false, bool r1=false, bool r2=false){
+		ofstream theFile(fileName);
+		theFile<<leftX<<","<<leftY<<","<<rightX<<","<<rightY<<","<<a<<","<<b<<","<<x<<","<<y<<","<<right<<","<<down<<","<<up<<","<<left<<","<<l1<<","<<l2<<","<<r1<<","<<r2<<",\n";
+		theFile.close();
+}
+
+void parseFileInput(string fileName,int array[16],int lineNumber){//this probably won't work as expected so be careful when testing
+	ifstream theFile(fileName);
+	char line[50];
+	for(int i=0;i<lineNumber;i++) theFile.get(line,50);
+	theFile.close();
+	char* t=strtok(line,",");
+	int number=0;
+	while(t!=NULL && number<16){
+		array[number]=(int)t;
+		number++;
+		t=strtok(NULL,",");
+	}
 }
