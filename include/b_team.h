7de05c6cf924a915a0c7a79b866ef954e8bb0ba1
Nublinel <coolcarter1017@gmail.com>
2025-01-27 16:31:12 -0800

    code_bot in main
    the code bot in main

--- include/b_team.h
+++ include/b_team.h
@@ -23,8 +23,8 @@ void auton() {
 void drive(auto master) {
 	int left = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 	int right = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-int triggle_up=master.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
-int triggle_down=master.get_digital(pros::E_CONTROLLER_DIGITAL_L1);;
+int triggle_up = master.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
+int triggle_down = master.get_digital(pros::E_CONTROLLER_DIGITAL_L1);;
 
 	left_front.move_voltage(left * MOVE_VOLT);
 	right_front.move_voltage(right * -MOVE_VOLT);
