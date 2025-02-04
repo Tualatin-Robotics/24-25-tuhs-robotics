477c93e2f54f17d947aaa605a40bce937cdfadf3
Nublinel <coolcarter1017@gmail.com>
2025-02-03 16:51:49 -0800

    Revert "claw for b team"
    This reverts commit 9cadd606b63eb7a9fb3f7f4d59b4aad9c50b4f5e.

--- include/b_team.h
+++ include/b_team.h
@@ -7,7 +7,6 @@ pros::Motor right_front(19);
 pros::Motor left_back(18);
 pros::Motor right_back(17);
 pros::Motor lift(10); 
-pros::Motor claw(15);
 
 void init() {
 	lift.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
@@ -24,11 +23,10 @@ void auton() {
 void drive(auto master) {
 	int left = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 	int right = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-	int triggle_up = master.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
-	int triggle_down = master.get_digital(pros::E_CONTROLLER_DIGITAL_L1);;
+int triggle_up = master.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
+int triggle_down = master.get_digital(pros::E_CONTROLLER_DIGITAL_L1);;
 
-	//left_front.move_voltage(left * MOVE_VOLT);
-	left_front.move(127);
+	left_front.move_voltage(left * MOVE_VOLT);
 	right_front.move_voltage(right * -MOVE_VOLT);
 
 	int left_trig = master.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
@@ -36,11 +34,6 @@ void drive(auto master) {
 	int left_bump = master.get_digital(pros::E_CONTROLLER_DIGITAL_L1);
 	int right_bump = master.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
 
-	int up = master.get_digital(pros::E_CONTROLLER_DIGITAL_UP);
-	int down = master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN);
-
-	lift.move_voltage(12000*(triggle_up-triggle_down));
-
-	claw.move_voltage(1200*(up-down));
+lift.move_voltage(12000*(triggle_up-triggle_down)); 
 
 }
